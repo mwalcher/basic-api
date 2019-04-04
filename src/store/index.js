1bf10b01b25f2993e79a36bf717ea5a2851f123e
Matthew Walcher <mwalcher@live.ca>
2019-04-04 08:28:52 -0400

    removed unused package

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,9 @@ import {
     compose
 } from 'redux';
 import thunk from 'redux-thunk';
-import { middleware as reduxPackMiddleware } from 'redux-pack';
 import reducer from '../reducers';
 
 export default createStore(
     reducer,
-    compose(applyMiddleware(thunk, reduxPackMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
